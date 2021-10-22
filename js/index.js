7f1472b66036321e50cd50835f0bd895c0905ba4
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-10-21 21:12:09 -0600

    website update

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
-// Declaring DOM elements
-const hamburgerBtn = document.getElementById('btn');
+import { classProjects } from "./projects/class-work.js";
 
-hamburgerBtn.addEventListener('click', () => {
-    console.log('✔️')
-    document.getElementById('ul-nav').classList.toggle('hide')
-})
+// Declaring DOM elements
+const hamburgerBtn = document.getElementById("btn");
+
+hamburgerBtn.addEventListener("click", () => {
+  console.log("✔️");
+  document.getElementById("ul-nav").classList.toggle("hide");
+});
+
+// poplulatingDOM
+
+// Jquery
+$(document).ready(function () {
+  console.log("🙃");
+
+  //   example of DRY coding in jquery
+  $(".cardBtn").on("click", function () {
+    var cardId = $(this).attr("data-panel");
+    // $('.card').find('div').toggle()
+
+    if ($(this).is(".special")) {
+      console.log("yerrr skraaa");
+    } else if ($(this).parent().is(".parent")) {
+      console.log("skra yerrrp");
+    }
+    console.log(cardId);
+  });
+});
---
+++ js/projects/class-work.js
@@ -0,0 +1,47 @@
+export const classProjects = [
+  {
+    id: 1,
+    name: "Project: Star Wars",
+    classCode: "DGM 1600, Utah Valley University",
+    classDate: "Spring 2021",
+    completionDate: "Spring 2021",
+    link: "https://www.dylnsworks.com/films/index.html",
+    imgURL: "../../img/starwarsProject-tommy-van-kessel.jpg",
+    caseStudyURL: "",
+    discription: {
+      shortDiscription:
+        "This class project was a introduction to Javascript functions that allow us to programmatically populate the DOM.",
+      fullDiscription: `Similary to how I'm structuring my site. This projecct used Javascript/JSON files to provide us the information to populate the DOM. Utalizing files that contained Star Wars asset information such as characters, vehicles, and planets. To access that information, we learned the basics of looping through arrays and objects, and then populating the DOM. I felt I was successful in the goal for this assignment, so I took the time to learn a bit of mobile responsiveness before it was taught to me. This helped introduce me to the world of responsive design and I did fail at it for this assignment, but its a foundation I got that set me up for success. On top of that, this was also exploration of creativity and trying to make this site my own, so I explored color themes and simple creative ways to make it unique, which gave me a introduction of designing a website with just code.`,
+    },
+  },
+  {
+    id: 2,
+    name: "People of Congress",
+    classCode: "DGM 1600, Utah Valley University",
+    classDate: "Spring 2021",
+    completionDate: "Spring 2021",
+    link: "https://www.dylnsworks.com/congress/index.html",
+    imgURL: "",
+    caseStudyURL: "",
+    discription: {
+      shortDiscription: `Continuing what I've learned from the starwars project. However, this more exploration of array methods and introducting ways to pogramatically update content on screen.`,
+      fullDiscription:
+        "This assignment was meant to explore array methods. We were given data that contained information about congress members (not the current congress) and we populated the DOM utalizing array methods. A bit on was the filter method and to properly use that to manipulate content on screen.",
+    },
+  },
+  {
+    id: 3,
+    name: "Pokemon Card Version: 1",
+    classCode: "DGM 1600, Utah Valley University",
+    classDate: "Spring 2021",
+    completionDate: "Spring 2021",
+    link: "https://www.dylnsworks.com/poke-card/index.html",
+    imgURL: "",
+    caseStudyURL: "",
+    discription: {
+      shortDiscription:
+        "Further developed my DOM manipulation skills with this project, but we used a real world API for this project. Resulting in my learning what APIs are and how to work with them.",
+      fullDiscription: `This was a true introduction to utalizing APIs. Using vanilla Javascript and the fetch API I learned how to grab data from an external server and populate a webpage via this method. Outcome of this project was knowledge of what APIs are, how to work with them, and other modern Javascript methods.`,
+    },
+  },
+];
