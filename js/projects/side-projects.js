de0ae4e908bc598d66520272ae1a32b1471443a4
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2021-10-22 12:35:24 -0600

    testForms

---
+++ js/projects/side-projects.js
@@ -0,0 +1,22 @@
+export const sideProjects = [
+  {
+    id: 1,
+    name: "Hatfield Photography",
+    startDate: "September 2021",
+    completeDate: "",
+    link: "",
+    imgURL: "",
+    caseStudyURL: "",
+    discription: "",
+  },
+  {
+    id: 2,
+    name: "BeatsByOnion",
+    startDate: "September 2021",
+    completeDate: "",
+    link: "",
+    imgURL: "",
+    caseStudyURL: "",
+    discription: "",
+  },
+];
