2f86fdeec8fac729e2c58eddc00302a62fdcfc28
Dylan Smith <78442693+ayodyln@users.noreply.github.com>
2022-02-03 23:03:48 -0700

    test

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
+
 // Declaring DOM elements
 const hamburgerBtn = document.getElementById("hamburgerBtn");
 
-hamburgerBtn.addEventListener("click", () => {
-  console.log("✔️");
+hamburgerBtn.addEventListener("click", (e) => {
   document.querySelector("#menuIcon").classList.toggle("fa-bars");
   document.querySelector("#menuIcon").classList.toggle("fa-times");
   document.getElementById("ulDiv").classList.toggle("open");
+  document.querySelector('main').scroll
 });
 
 // poplulatingDOM
